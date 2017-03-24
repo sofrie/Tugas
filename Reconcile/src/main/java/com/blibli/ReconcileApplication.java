707c69e6ce00f312ae0d9df54ba03a5e2539eb01
sofrie <nagimagi47@gmail.com>
2017-03-24 13:58:52 +0700

    Membuat project Reconcile

---
+++ Reconcile/src/main/java/com/blibli/ReconcileApplication.java
@@ -0,0 +1,12 @@
+package com.blibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconcileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconcileApplication.class, args);
+	}
+}
---
+++ Reconcile/src/test/java/com/blibli/ReconcileApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blibli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReconcileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
